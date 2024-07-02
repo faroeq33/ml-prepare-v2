6a17eb3e2d3249e904b5822e3f66934d901b8345
Faroeq <alves_1044@hotmail.com>
2024-07-02 12:16:51 +0200

    Update comments for creating handlandmarker

--- src/utils/createHandLandmarker.ts
+++ src/utils/createHandLandmarker.ts
@@ -1,9 +1,12 @@
 import { FilesetResolver, HandLandmarker } from "@mediapipe/tasks-vision";
 
 const createHandLandmarker = async () => {
+  // Load the vision tasks fileset resolver
   const vision = await FilesetResolver.forVisionTasks(
     "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
   );
+
+  // Create a hand landmarker instance with specified options
   const handLandmarker = await HandLandmarker.createFromOptions(vision, {
     baseOptions: {
       modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
@@ -12,15 +15,9 @@ const createHandLandmarker = async () => {
     runningMode: "VIDEO",
     numHands: 1,
   });
+
+  // Return the created hand landmarker instance
   return handLandmarker;
-  //   if (!landmarkerRef.current) {
-  //     console.log("something");
-  //     landmarkerRef.current = handLandmarker;
-  //     console.log("handlandmarker is created!");
-  //   }
-  // start capturing - zie hieronder
 };
 
 export default createHandLandmarker;
-// write so it can only make 1 drawing util instance
-// drawingUtilsRef.current = new DrawingUtils(ctx);
