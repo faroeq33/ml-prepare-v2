980bdd554aaa583b89344db67317c3ec4c2718ac
Faroeq <alves_1044@hotmail.com>
2024-07-11 12:39:20 +0200

    Rename message component & move folder

--- src/components/sections/ModelSettingsSection.tsx
+++ src/components/sections/ModelSettingsSection.tsx
@@ -1,13 +1,4 @@
-import {
-  Bird,
-  CornerDownLeft,
-  Mic,
-  Paperclip,
-  Rabbit,
-  Settings,
-  Share,
-  Turtle,
-} from "lucide-react";
+import { Bird, Rabbit, Settings, Share, Turtle } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -27,15 +18,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import NavSection from "./NavSection";
-import OutputBadge from "../ui/badges/output-badge";
 
 export function ModelSettingsSection() {
   return (
@@ -139,28 +123,6 @@ export function ModelSettingsSection() {
                       <Input id="top-k" type="number" placeholder="0.0" />
                     </div>
                   </fieldset>
-                  <fieldset className="grid gap-6 p-4 border rounded-lg">
-                    <legend className="px-1 -ml-1 text-sm font-medium">
-                      Messages
-                    </legend>
-                    <div className="grid gap-3">
-                      <Label htmlFor="role">Role</Label>
-                      <Select defaultValue="system">
-                        <SelectTrigger>
-                          <SelectValue placeholder="Select a role" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectItem value="system">System</SelectItem>
-                          <SelectItem value="user">User</SelectItem>
-                          <SelectItem value="assistant">Assistant</SelectItem>
-                        </SelectContent>
-                      </Select>
-                    </div>
-                    <div className="grid gap-3">
-                      <Label htmlFor="content">Content</Label>
-                      <Textarea id="content" placeholder="You are a..." />
-                    </div>
-                  </fieldset>
                 </form>
               </DrawerContent>
             </Drawer>
@@ -198,7 +160,7 @@ export function ModelSettingsSection() {
                             <Rabbit className="size-5" />
                             <div className="grid gap-0.5">
                               <p>
-                                Neural{" "}
+                                Neural
                                 <span className="font-medium text-foreground">
                                   Genesis
                                 </span>
@@ -253,46 +215,8 @@ export function ModelSettingsSection() {
               </form>
             </div>
             <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
-              <OutputBadge />
-
               <div className="flex-1" />
-              <form
-                className="relative overflow-hidden border rounded-lg bg-background focus-within:ring-1 focus-within:ring-ring"
-                x-chunk="dashboard-03-chunk-1"
-              >
-                <Label htmlFor="message" className="sr-only">
-                  Message
-                </Label>
-                <Textarea
-                  id="message"
-                  placeholder="Type your message here..."
-                  className="p-3 border-0 shadow-none resize-none min-h-12 focus-visible:ring-0"
-                />
-                <div className="flex items-center p-3 pt-0">
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button variant="ghost" size="icon">
-                        <Paperclip className="size-4" />
-                        <span className="sr-only">Attach file</span>
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent side="top">Attach File</TooltipContent>
-                  </Tooltip>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button variant="ghost" size="icon">
-                        <Mic className="size-4" />
-                        <span className="sr-only">Use Microphone</span>
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent side="top">Use Microphone</TooltipContent>
-                  </Tooltip>
-                  <Button type="submit" size="sm" className="ml-auto gap-1.5">
-                    Send Message
-                    <CornerDownLeft className="size-3.5" />
-                  </Button>
-                </div>
-              </form>
+              {/* <MessageForm /> */}
             </div>
           </main>
         </div>
