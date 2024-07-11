acb959aa2062f44f6c75b1e9110a6583b9227b9c
Faroeq <alves_1044@hotmail.com>
2024-07-11 10:45:22 +0200

    Rearrange layout for a settings form

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import React from "react";
 import { PoseProvider } from "./context/PoseContext.tsx";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
