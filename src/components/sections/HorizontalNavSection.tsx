47f2995f0910074fbf24bf3359ac70225bc0dc74
Faroeq <alves_1044@hotmail.com>
2025-02-05 22:27:06 +0100

    change favicon & tab title for contextual clarity

--- src/components/sections/HorizontalNavSection.tsx
+++ src/components/sections/HorizontalNavSection.tsx
@@ -22,7 +22,7 @@ function HorizontalNavSection() {
   return (
     <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b bg-background px-4">
       <h1 className="w-full text-xl font-semibold text-center">
-        ML prepare v2
+        ML prepare v2 🏋🏾‍♂️
       </h1>
       <Drawer>
         <DrawerTrigger asChild>
