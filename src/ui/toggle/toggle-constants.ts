a0fed39165cf944cf7b8cfb4e0b658bcd7c1fbf2
Faroeq <alves_1044@hotmail.com>
2025-02-14 11:44:55 +0100

    Remove toggle & date-fns

--- src/ui/toggle/toggle-constants.ts
+++
@@ -1,7 +0,0 @@
-import { type VariantProps } from "class-variance-authority";
-import { toggleVariants } from "./toggle-variants";
-
-export type ToggleProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
-  VariantProps<typeof toggleVariants> & {
-    asChild?: boolean;
-  };
