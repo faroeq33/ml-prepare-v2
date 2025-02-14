b737edf9fd0e3b82adcaf199eb62220a81aab752
Faroeq <alves_1044@hotmail.com>
2025-02-14 12:04:31 +0100

    refactor label + button

--- src/ui/label.tsx
+++ src/ui/label.tsx
@@ -1,19 +1,21 @@
-import { forwardRef, ElementRef, ComponentPropsWithoutRef } from "react";
-import { Root } from "@radix-ui/react-label";
-import { cva, type VariantProps } from "class-variance-authority";
+import * as LabelPrimitive from "@radix-ui/react-label";
 
 import { cn } from "@/utils/cn";
 
-const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-);
-
-const Label = forwardRef<
-  ElementRef<typeof Root>,
-  ComponentPropsWithoutRef<typeof Root> & VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <Root ref={ref} className={cn(labelVariants(), className)} {...props} />
-));
-Label.displayName = Root.displayName;
+function Label({
+  className,
+  ...props
+}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+  return (
+    <LabelPrimitive.Root
+      data-slot="label"
+      className={cn(
+        "text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+        className
+      )}
+      {...props}
+    />
+  );
+}
 
 export { Label };
